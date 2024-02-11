69afd7cb34f9af0fe459c397a2d45ec1f5b9dcd4
Timber-min <44857899+Timber-min@users.noreply.github.com>
2024-02-11 13:55:17 +0900

    Add file read and parsing

---
+++ src/indicator.py
@@ -0,0 +1,38 @@
+def readAnswerFile():
+    with open("resources/answer.csv", "r", encoding="UTF-8") as file:
+        return file.readlines()
+
+
+class Answer:
+    def __init__(self, kor, eng):
+        self.kor = kor
+        self.eng = eng
+
+    def cmpKor(self, other):
+        return self.kor == other
+
+    def cmpEng(self, other):
+        return self.eng == other
+
+    def __repr__(self):
+        return "[정답: 국문명 %s, 영문명 %s]" % (self.kor, self.eng)
+
+    def __str__(self):
+        return self.__repr__()
+
+
+def parse(lines):
+    lines = lines[1:]  # erase header of the csv file
+    ret = dict()
+
+    for line in lines:
+        imageID, indiID, kor, eng = line.strip().split(",")
+        if imageID not in ret:
+            ret[imageID] = dict()
+        ret[imageID][indiID] = Answer(kor, eng)
+
+    return ret
+
+
+if __name__ == "__main__":
+    print(parse(readAnswerFile()))
